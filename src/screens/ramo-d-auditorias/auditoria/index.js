44d89febcb84a4db97e5e8cfc3bffec2359b8d59
ViniciusDominguesEasy <134722839+ViniciusDominguesEasy@users.noreply.github.com>
2024-07-04 02:51:46 -0300

    Finaliza tela de audiorias

--- src/screens/ramo-d-auditorias/auditoria/index.js
+++ src/screens/ramo-d-auditorias/auditoria/index.js
@@ -2,6 +2,7 @@ import "./auditoria.css"
 import Bannerhome from '../../../components/bannerhome'
 import Rodape from '../../../components/rodape'
 import TituloPagina from "../../../components/titulopagina";
+import Card from "../../../components/card";
 
 function Auditoria(){
 
@@ -9,7 +10,10 @@ function Auditoria(){
         <>
         <Bannerhome />
         <TituloPagina titulopagina="Auditorias" descricaotitulo="Visualiação por colaborador e por curso" tembotao={false} botao="Ver"/>
-        ate
+        <div className='home-content'>
+            <Card tipo="Por treinamento" descricao="Auditorias de treinamentos"/>
+            <Card tipo="Por colaborador" descricao="Auditorias de colaboradores"/>
+      </div>
         <Rodape />
         </>
 
