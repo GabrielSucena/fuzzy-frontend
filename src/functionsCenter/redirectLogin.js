dfc36a52f666d6b626399947b12eece0b116393b
Vinicius Domingues <vinii.domingues@gmail.com>
2024-09-16 21:05:06 -0300

    Corrige inicialização do login

---
+++ src/functionsCenter/redirectLogin.js
@@ -0,0 +1,10 @@
+// src/RedirectToLogin.js
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const RedirectToLogin = () => {
+
+  return null; // Este componente não renderiza nada
+};
+
+export default RedirectToLogin;
